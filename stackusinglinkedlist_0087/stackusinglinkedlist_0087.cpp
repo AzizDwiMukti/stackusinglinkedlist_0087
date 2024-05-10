906fae7200304e0bd69b224dbdec85f559815ae9
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-10 13:41:46 +0700

    memperbaiki eror

--- stackusinglinkedlist_0087/stackusinglinkedlist_0087.cpp
+++ stackusinglinkedlist_0087/stackusinglinkedlist_0087.cpp
@@ -38,7 +38,7 @@ public:
 		if (isEmpty()) {
 			cout << "Stack is empty." << endl;
 		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next; //Update the top pointer to the next node
 	}
-	cout << "Popped value: " << top->data << endl;
-	top = top->next; //Update the top pointer to the next node
 };
